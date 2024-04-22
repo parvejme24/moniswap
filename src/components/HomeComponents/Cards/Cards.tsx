f18cf98b110c8f87d5514c4faf060180860510fb
parvejme24 <mdparvejme24@gmail.com>
2024-04-22 21:07:36 +0600

    update onboarding section

--- src/components/HomeComponents/Cards/Cards.tsx
+++ src/components/HomeComponents/Cards/Cards.tsx
@@ -36,22 +36,24 @@ export default function Cards() {
 
   return (
 
-<div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
-  {
-    items.map(item => <div key={item.id} className=" text-white w-[284px]">
-    <div className=" bg-[#1E1E1E] p-5  rounded-tl-[15px] rounded-tr-[15px] h-[246px]">
-    <span className=" flex justify-between items-center ">
-         <h3 className=" text-xl">{item?.title}</h3>
-         <img src={CARD_LOGO} alt="" />
-       </span>
-       <p className=" pt-3 text-sm text-[#CFCFCF]">{item?.description}</p>
-     
+    <div className="container mx-auto py-24">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {
+          items.map(item => <div key={item.id} className=" mx-10 md:mx-5 lg:mx-0 text-white">
+            <div className=" bg-[#1E1E1E] p-5  rounded-tl-[15px] rounded-tr-[15px] h-[246px]">
+              <span className=" flex justify-between items-center ">
+                <h3 className=" text-xl">{item?.title}</h3>
+                <img src={CARD_LOGO} alt="" />
+              </span>
+              <p className=" pt-3 text-sm text-[#CFCFCF]">{item?.description}</p>
+
+            </div>
+            <div className=" bg-[#FFB443] rounded-bl-[15px] rounded-br-[15px] h-[246px]">
+              <img src={item?.cardImage} alt={item?.title} />
+            </div>
+          </div>)
+        }
+      </div>
     </div>
-    <div className=" bg-[#FFB443] rounded-bl-[15px] rounded-br-[15px] h-[246px]">
-      <img src={item?.cardImage} alt={item?.title} />
-    </div>
-   </div>)
-  }
-</div>
   )
 }
--- src/components/HomeComponents/Onboarding/Onboarding.tsx
+++ src/components/HomeComponents/Onboarding/Onboarding.tsx
@@ -1,20 +1,24 @@
 import BG_ONBOARDING from '../../../assets/bg-onboarding.png';
 import RINGS from "../../../assets/rings.png";
-import ONBOARDING from '../../../assets/onboarding.png'
+import ROCKET from '../../../assets/rocket.png'
 import DWON_ARROW from '../../../assets/down-arrow.png'
 
 export default function Onboarding() {
   return (
-    <div className="relative h-screen bg-center p-10" style={{backgroundImage: `url(${BG_ONBOARDING})`}}>
-              <img className=' mx-auto mb-10' src={DWON_ARROW} alt="" />
+    <div className="relative h-screen bg-center p-10" style={{ backgroundImage: `url(${BG_ONBOARDING})` }}>
+      <img className=' mx-auto mb-10' src={DWON_ARROW} alt="" />
 
       <div className="absolute inset-0 flex justify-center items-center">
-        <div className="text-white text-center">
-
-        <img className=' mx-auto' src={RINGS} alt="" />
-        <p className="mt-2">Looking to get</p>
-        <h1 className="text-4xl font-bold ">started on Moniswap?</h1>
-        <img className=' mx-auto my-8' src={ONBOARDING} alt="" />
+        <div className="text-white text-center space-y-3">
+          <img className='drag-none mx-auto' src={RINGS} alt="" />
+          <p className="mt-2">Looking to get</p>
+          <h2 className="text-3xl md:text-4xl italic font-semibold pb-8">Started on Moniswap?</h2>
+          <div className='w-32 h-32 mx-auto rounded-full bg-gradient-to-b from-[#ffbf60] to-[#fcb415] flex items-center justify-center'>
+            <div>
+              <img className='mx-auto mb-2' src={ROCKET} alt="" />
+              <span className='text-white'>Swap</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
