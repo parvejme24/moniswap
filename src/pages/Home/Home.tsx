f003e05b29eacc60b23c4b1f9822bb74952bccd9
parvejme24 <mdparvejme24@gmail.com>
2024-04-22 13:46:01 +0600

    added features section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Banner from "../../components/HomeComponents/Banner/Banner";
+import Features from "../../components/HomeComponents/Features/Features";
 
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className="container max-w-5xl mx-auto">
       <Banner />
+      <Features />
     </div>
   )
 }
