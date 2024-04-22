d8a72a3610022ca69e2d520c0bd9bcf71771515c
parvejme24 <mdparvejme24@gmail.com>
2024-04-22 13:47:54 +0600

    update features.tsx file and use functional component

--- src/components/HomeComponents/Features/Features.tsx
+++ src/components/HomeComponents/Features/Features.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import CHECK from '../../../assets/check.png'
 
 // Define an interface for feature items
@@ -17,7 +16,8 @@ const featuresData: FeatureItem[] = [
   { text: 'Anti-dilution Rebases for Voters', icon: CHECK },
 ];
 
-const MoniswapFeaturesSection: React.FC = () => {
+
+export default function Features() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -32,6 +32,5 @@ const MoniswapFeaturesSection: React.FC = () => {
       </div>
     </div>
   );
-};
+}
 
-export default MoniswapFeaturesSection;
