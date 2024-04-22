1dead4109fae854081a4dadd3cb5f12d703faed7
parvejme24 <mdparvejme24@gmail.com>
2024-04-22 12:48:34 +0600

    addd banner section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import Banner from "../../components/HomeComponents/Banner/Banner";
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="container mx-auto">
+      <Banner />
+    </div>
   )
 }
