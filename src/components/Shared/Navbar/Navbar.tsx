445d5773d3afc5888cf722409cf5e0779336d728
parvejme24 <mdparvejme24@gmail.com>
2024-04-22 19:04:48 +0600

    update navbar

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -124,7 +124,7 @@ const Navbar: React.FC = () => {
               <button className='py-3 px-6 rounded-lg bg-gradient-to-r from-[#ffbf60] to-[#fcb415] text-white'>Contact</button>
             </div>
             <div className="flex-shrink-0 md:hidden">
-              <button onClick={toggleMobileMenu} className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              <button onClick={toggleMobileMenu} className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M3 9a1 1 0 0 1 1-1h12a1 1 0 0 1 0 2H4a1 1 0 0 1-1-1zm0-4a1 1 0 0 1 1-1h12a1 1 0 0 1 0 2H4a1 1 0 0 1-1-1zm0 8a1 1 0 0 1 1-1h12a1 1 0 0 1 0 2H4a1 1 0 0 1-1-1z" />
                 </svg>
@@ -138,8 +138,8 @@ const Navbar: React.FC = () => {
               <div className="rounded-lg shadow-l text-white ring-1 ring-black ring-opacity-5 overflow-hidden">
                 <div className="px-5 pt-4 flex items-center justify-between">
                   <div></div>
-                  <div className="-mr-2">
-                    <button onClick={toggleMobileMenu} type="button" className="bg-black rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
+                  <div className="">
+                    <button onClick={toggleMobileMenu} type="button" className="bg-[#1E1E1E] hover:bg-[#1E1E1E] rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
                       <span className="sr-only">Close menu</span>
                       <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
@@ -147,7 +147,7 @@ const Navbar: React.FC = () => {
                     </button>
                   </div>
                 </div>
-                <div className="px-2 pt-2 pb-3 mt-5 bg-black">
+                <div className="px-2 pt-2 pb-3 mt-10 bg-black">
                   <ul className='space-y-2 z-10'>
                     {navItemsMobile}
                   </ul>
